4382d3bcf1b8454bb6d0a7fe2d373cbafecd6347
CaraMc86 <150253604+CaraMc86@users.noreply.github.com>
2024-06-13 15:13:15 +0100

    created method for about section nd heper ethods to capture sections required, converted into string. One test is failing due to a single doubble space but otherwise all is working fine

--- get_plant_info.py
+++ get_plant_info.py
@@ -14,6 +14,10 @@ class GetPlant:
         # Initialising about_list_items as an empty list
         self.about_list_items = []
         self.position = None
+        self.soil = None
+        self.growthRate = None
+        self.hardiness = None
+        self.homeCare = None
 
     def get_url_info(self):
         """Method to parse HTML from URL to be used in other methods"""
@@ -80,48 +84,28 @@ class GetPlant:
             self.position = str(self.position)
         return self.position
 
+    def _get_soil(self):
+        if self.soil is None:
+            self.soil = [li.get_text(strip=True).replace("Soil:", "").strip() for li in self.about_list_items if
+                             li.get_text(strip=True).startswith("Soil:")]
+            self.soil = self.soil[0] if self.soil else 'N/A'
+            self.soil = str(self.soil)
+            return self.soil
 
-    #     if about_description_span:
-    #         list_items = about_description_span.find_all('li')
-    #
-    #         # Extract text from each list item, filter li items based on the required keywords they start with and then remove these keywords prefixes to store in a variable.
-    #         # This has been done as there are no attributes for the individual list items to target directly when scraping
-    #         position_items = [li.get_text(strip=True).replace("Position:", "").strip() for li in list_items if
-    #                           li.get_text(strip=True).startswith("Position:")]
-    #         plant_position = position_items[0] if position_items else 'N/A'
-    #         print(f'Plant Position {plant_position}')
-    #
-    #         soil_items = [li.get_text(strip=True).replace("Soil:", "").strip() for li in list_items if
-    #                       li.get_text(strip=True).startswith("Soil:")]
-    #         soil_type = soil_items[0] if soil_items else 'N/A'
-    #         print(f'Soil Type {soil_type}')
-    #
-    #         growthRate_items = [li.get_text(strip=True).replace("Rate of growth:", "").strip() for li in list_items if
-    #                             li.get_text(strip=True).startswith("Rate of growth:")]
-    #         growth_rate = growthRate_items[0] if growthRate_items else 'N/A'
-    #         print(f'Growth rate: {growth_rate}')
-    #
-    #         hardiness_items = [li.get_text(strip=True).replace("Hardiness:", "").strip() for li in list_items if
-    #                            li.get_text(strip=True).startswith("Hardiness:")]
-    #         plant_hardiness = hardiness_items[0] if hardiness_items else 'N/A'
-    #         print(f'Hardiness: {plant_hardiness}')
-    #
-    #         # Extract text from each list item, filter items starting with "Home Care:", and remove "Home Care:" prefix to store in a variable
-    #         homeCare_items = [li.get_text(strip=True).replace("Home care:", "").strip() for li in list_items if
-    #                           li.get_text(strip=True).startswith("Home care:")]
-    #         home_care = homeCare_items[0] if homeCare_items else 'N/A'
-    #         print(f'Home Care: {home_care}')
-    #
-    #         hazards_span = soup.find('span', class_='item-poisonous')
-    #         if hazards_span:
-    #             hazard_li = hazards_span.find_all('li')
-    #             for li in hazard_li:
-    #                 # Check if the text starts with any of the specified prefixes
-    #                 if li.get_text(strip=True).startswith(('Humans/Pets:', 'Pets:', 'Humans:')):
-    #                     plant_hazard = li.get_text(strip=True).strip()
-    #                     break  # Exit the loop once a match is found
-    #             else:
-    #                 plant_hazard = 'N/A'  # Set default value if no match is found
-    #         else:
-    #             plant_hazard = 'N/A'
-    #         print(f'Hazards: {plant_hazard}')
+    def _get_growthRate(self):
+        if self.growthRate is None:
+            self.growthRate = [li.get_text(strip=True).replace("Rate of growth:", "").strip() for li in self.about_list_items if
+                             li.get_text(strip=True).startswith("Rate of growth:")]
+            self.growthRate = self.growthRate[0] if self.growthRate else 'N/A'
+            self.growthRate = str(self.growthRate)
+            return self.growthRate
+
+    def _get_homeCare(self):
+        if self.homeCare is None:
+            self.homeCare = [li.get_text(strip=True).replace("Home care:", "").strip().replace("  ", " ").strip() for li in self.about_list_items if
+                             li.get_text(strip=True).startswith("Home care:")]
+            self.homeCare = self.homeCare[0] if self.homeCare else 'N/A'
+            self.homeCare = str(self.homeCare)
+            return self.homeCare
+
+
--- test_getaboutinfo.py
+++ test_getaboutinfo.py
@@ -35,6 +35,54 @@ def test_get_position(plant_info):
     assert position.strip(), "Position should not be an empty string"
     assert position == expected_position
 
+def test_get_soil(plant_info):
+    # Setup
+    instance = plant_info
+
+    # Act
+    instance.get_url_info()
+    instance.get_about_info()
+    soil = instance._get_soil()
+    expected_soil = 'Peat free, general-purpose potting compost'
+
+    # Assert
+    assert isinstance(soil, str), "Soil should be a string"
+    assert soil != 'N/A', "Soil should not be 'N/A'"
+    assert soil.strip(), "Soil should not be an empty string"
+    assert soil == expected_soil
+
+def test_get_growthRate(plant_info):
+    # Setup
+    instance = plant_info
+
+    # Act
+    instance.get_url_info()
+    instance.get_about_info()
+    growth_rate = instance._get_growthRate()
+    expected_growth_rate = 'Average to fast'
+
+    # Assert
+    assert isinstance(growth_rate, str), "Growth rate should be a string"
+    assert growth_rate != 'N/A', "Growth rate should not be 'N/A'"
+    assert growth_rate.strip(), "Growth rate should not be an empty string"
+    assert growth_rate == expected_growth_rate
+
+def test_get_homeCare(plant_info):
+    # Setup
+    instance = plant_info
+
+    # Act
+    instance.get_url_info()
+    instance.get_about_info()
+    home_care = instance._get_homeCare()
+    expected_home_care = 'Keep it out of direct sunlight, ideally in a bright spot, and mist the leaves, particularly if the room is warm and dry. Also the leaves will appreciate being given a wash sometimes to keep them clean and dust free. This plant likes a moist soil but not a waterlogged soil, so in the winter feel the soil before you water it. In the summer you can water more freely.'
+
+    # Assert
+    assert isinstance(home_care, str), "Home care should be a string"
+    assert home_care != 'N/A', "Home care should not be 'N/A'"
+    assert home_care.strip(), "Home care should not be an empty string"
+    assert home_care == expected_home_care
+
 # Run the tests using pytest
 if __name__ == "__main__":
     pytest.main()
